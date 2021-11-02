d29ec11c161bc1bb954c21069be121be1f426a10
khaled samra <khalid.samraa@gmail.com>
2021-11-03 00:56:27 +0200

    Revert "uploading the theme"
    This reverts commit 6c0e75234e073060081aad71803efca14c457cf6.

--- templates/customers-archive/section-hero.php
+++
@@ -1,9 +0,0 @@
-<?php 
-global $index,$sections;
-$data=$sections[$index];
-?>
-<section class="section customer-stories-archive-hero">
-  <div class="container">
-    <h2><?php echo $data['title']; ?></h2>
-  </div>
-</section>
